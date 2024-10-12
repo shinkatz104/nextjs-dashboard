9cd0da4c43865a0754c835674db577ae361fe6a2
shinkatz104 <shinkatz104@gmail.com>
2024-10-12 20:57:02 +0900

    chapter9: add loading skeleton for only LatestInvoices component on dashboard overview page

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,13 +2,13 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data'; // remove fetchRevenue
+import { fetchCardData } from '@/app/lib/data'; // remove fetchRevenue, fetchLatestInvoices
 import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { RevenueChartSkeleton, LatestInvoicesSkeleton } from '@/app/ui/skeletons';
  
 export default async function Page() {
   // const revenue = await fetchRevenue(); // remove fetchRevenue
-  const latestInvoices = await fetchLatestInvoices();
+  // const latestInvoices = await fetchLatestInvoices(); // remove fetchLatestInvoices
   const {
     numberOfInvoices,
     numberOfCustomers,
@@ -36,7 +36,10 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
